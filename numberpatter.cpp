defa35bdd22787397b210064f400ea059fcee97e
aditya <aditya_chandra@srmap.edu.in>
2024-08-23 00:01:54 +0530

    numberpattern

--- numberpatter.cpp
+++ numberpatter.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-int i,j;
-int n=5,k=1;
+int n=5;
+int x=1;
 for(int i=1;i<=n;i++){
-    for(j=1;j<=n;j++){
-        cout<<k++<<"";
+    for(int j=1;j<=n;j++){
+        cout<<x<<"";
+        x+=2;
     }
     cout<<endl;
 }
