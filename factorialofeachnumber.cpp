6550f94421cfd342fd10c50d5fcb9a22131ca41d
aditya <aditya_chandra@srmap.edu.in>
2024-08-24 23:59:53 +0530

    hrllanfc

---
+++ factorialofeachnumber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+int cn;
+int i,j;
+long int fact;
+cout<<"Enter the number: ";
+cin>>n;
+cout<<"Factorial of each number from 1 to "<<n<<" is: "<<endl;
+cout<<"numnber:factorial"<<endl;
+for(i<1;i<=n;i++){
+    cn=i;
+    fact=1;
+    for(j=1;j<=cn;j++){
+        fact=fact*j;
+    }
+    cout<<cn<<":"<<fact<<endl;
+}
+return 0;
+}
