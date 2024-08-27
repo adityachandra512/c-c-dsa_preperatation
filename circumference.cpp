9147a7190e0f7775cc763ea563b7110626ce2881
aditya <aditya_chandra@srmap.edu.in>
2024-08-27 23:38:33 +0530

    circumference

---
+++ circumference.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int r;
+float c;
+cout<<"Enter the radius of the circle: ";
+cin>>r;
+c=2*3.14*r;
+cout<<"The circumference of the circle is: "<<c<<endl;
+return 0;
+}
