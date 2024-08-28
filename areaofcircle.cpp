afe48f4e5802aab725f0828e605cc35ac6933d2a
aditya <aditya_chandra@srmap.edu.in>
2024-08-28 22:48:25 +0530

    circle and triangle area

---
+++ areaofcircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int r;
+float a;
+cout<<"Enter the radius of the circle: ";
+cin>>r;
+a=3.14*r*r;
+cout<<"The area of the circle is: "<<a<<endl;
+return 0;
+}
---
+++ areaoftriangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int b,h,area;
+cout<<"Enter the base of the triangle: ";
+cin>>b;
+cout<<"Enter the height of the triangle: ";
+cin>>h;
+area=(b*h)/2;
+cout<<"The area of the triangle is: "<<area<<endl;
+return 0;
+}
