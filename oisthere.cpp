7cbf57e7aac722b5a63b58b4adb6d3da743411b9
aditya <aditya_chandra@srmap.edu.in>
2024-08-31 22:58:53 +0530

    o is there

---
+++ oisthere.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"pl enter o";
+cin>>a;
+if(a==0){
+    cout<<"0 is there";
+}
+else{
+    cout<<"0 is not there";
+}
+return 0;
+}
