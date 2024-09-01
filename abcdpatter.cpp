a4245e35a9f724c5d801574fe1901c3ca6be4c89
aditya <aditya_chandra@srmap.edu.in>
2024-09-01 23:18:51 +0530

    abcdpatter

---
+++ abcdpatter.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+char i,j;
+for(i='A';i<='E';i++){
+    for(j='A';j<='E';j++){
+        cout<<char(i);
+    }
+    cout<<endl;
+}
+return 0;
+}
