3ead015845dcfcf655c7c1df9377d66d5daf499e
aditya <aditya_chandra@srmap.edu.in>
2024-09-05 23:47:48 +0530

    volumeofcuboid

---
+++ volumeofcuboid.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int l,b,h;
+cout<<"Enter the length of the cuboid: ";
+cin>>l;
+cout<<"Enter the breadth of the cuboid: ";
+cin>>b;
+cout<<"Enter the height of the cuboid: ";
+cin>>h;
+cout<<"The volume of the cuboid is: "<<l*b*h<<endl;
+return 0;
+}
